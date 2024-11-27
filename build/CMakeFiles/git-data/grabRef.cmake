984889e8c419e40e2e852b18cb32499e7bb3b8a8
Daniel Saravia <saraviad4444@gmail.com>
2024-11-27 12:57:42 -0700

    start

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "/home/danielsaravia/Desktop/cram/blink/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "/home/danielsaravia/Desktop/esp32/.git")
+set(GIT_DIR "/home/danielsaravia/Desktop/cram/blink/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
